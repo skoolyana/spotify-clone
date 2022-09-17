9502fba2a46d529561f65f2fe48678d93bae7c5f
unknown <kulyanasun@gmail.com>
2022-09-17 14:37:08 +0200

    Commit

--- components/Player.js
+++ components/Player.js
@@ -4,7 +4,19 @@ import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { currentTrackState, isPlayingState } from '../atoms/songAtom';
 import useSongInfo from '../hooks/useSongInfo';
-import useSpotify from '../hooks/useSpotify'
+import useSpotify from '../hooks/useSpotify';
+import { VolumeUpIcon as VolumeDownIcon } from '@heroicons/react/outline';
+import { VolumeUpIcon } from '@heroicons/react/outline';
+
+import {
+  RewindIcon,
+  PauseIcon,
+  PlayIcon,
+  FastForwardIcon,
+  ReplyIcon,
+  SwitchHorizontalIcon,
+} from '@heroicons/react/solid';
+
 
 function Player() {
   
@@ -41,6 +53,26 @@ function Player() {
    };
      
 
+   const handlePlayPause = () => {
+
+     spotifyApi.getMyCurrentPlaybackState().then((data)=> {
+    if(data.body.is_playing) {
+
+      spotifyApi.pause();
+      setIsPlaying(false);
+
+    }
+    else
+    {
+      spotifyApi.play();
+      setIsPlaying(true);
+    }
+
+     });
+
+
+   };
+
 
   useEffect(()=> {
 
@@ -64,18 +96,42 @@ function Player() {
   const songInfo = useSongInfo();
 
     return (
-    <div>
+    <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
         
-        {
-            /* Left */
-        }
-        
-        <div>
+       
+        <div className='flex items-center space-x-4'>
 
         <img className='hidden md:inline h-10 w-10'  src={songInfo?.album?.images?.[0]?.url}></img>
 
+        <div>
+          <h3>
+            {songInfo?.name}
+          </h3>
+          <p>{songInfo?.artists?.[0]?.name}</p>
+        </div>
         </div>
         
+        {
+
+          <div className='flex items-center justify-evenly'>
+
+            <SwitchHorizontalIcon className='button'></SwitchHorizontalIcon>
+            <RewindIcon className='button'></RewindIcon>
+            {
+              isPlaying ? (
+                <PauseIcon onClick={handlePlayPause} className='button w-10 h-10'></PauseIcon>
+              ):(<PlayIcon onClick={handlePlayPause} className='button w-10 h-10'></PlayIcon>)
+
+
+            }
+
+
+            <PlayIcon className='button'></PlayIcon>
+            <FastForwardIcon className='button'></FastForwardIcon>
+            <ReplyIcon className='button'></ReplyIcon>
+          </div>
+
+        }
         
         </div>
   )
